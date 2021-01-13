7bac8032cb74870436ec708f578eb09779c1b5fa
vrlelif <57722793+vrlelif@users.noreply.github.com>
2021-01-13 17:26:24 +0300

    Add files via upload

---
+++ htmlToLetter.js
@@ -0,0 +1,19 @@
+
+function clean(string) {
+    return string   
+    
+    .replace(/&#199;/g, "Ç")
+    .replace(/&#231;/g, "ç")
+    .replace(/&#286;/g, "Ğ")
+    .replace(/&#287;/g, "ğ")
+    .replace(/&#304;/g, "İ")
+    .replace(/&#305;/g, "ı")
+    .replace(/&#214;/g, "Ö")
+    .replace(/&#246;/g, "ö")
+    .replace(/&#220;/g, "Ü")
+    .replace(/&#252;/g, "ü")
+    .replace(/&#350;/g, "Ş")
+    .replace(/&#351;/g, "ş")
+}
+
+ 
